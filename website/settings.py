515e5b362dcb5073cf85650f1c4b5cac4001f729
James Caldwell <james.caldwell.1981@gmail.com>
2022-10-17 16:42:55 -0400

    added host to ALLOWED_HOSTS

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = environ['DJANGO_SECRET']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jamescaldwell1981.pythonanywhere.com']
 
 
 # Application definition
